348f30933f7928c572f6e3da46fa6c6c7e5cbfb8
Piotr Rosa <piotr.rosa@sabre.com>
2020-01-19 16:06:59 +0100

    test

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express()
 const port = 8081
 const _ = require('underscore');
 
+const insert = require('./insert');
+
+const COLLECTION_NAME = 'kobylany'
 app.get('/', (req, res) => {
  res.sendStatus(404); 
 });
@@ -11,9 +14,15 @@ app.get('/kobylany', (req, res) => {
 	console.log(req.query);
 	if(req.query.valuename === 'PM25') {
 		console.log('PM 2.5', req.query.value);
+		insert.insertToDb(COLLECTION_NAME, {
+			'PM2.5' : req.query.value
+		})
 	}
 	if(req.query.valuename === 'PM10') {
 		console.log('PM 10', req.query.value);
+		insert.insertToDb(COLLECTION_NAME, {
+			'PM10' : req.query.value
+		})
 	}
 	res.sendStatus(200);
 });
--- insert.js
+++ insert.js
@@ -4,7 +4,7 @@ const assert = require('assert');
 const url = 'mongodb://localhost:27017';
 const dbName = 'airQuality';
 
-const insert = (async function (collectionName) {
+const insert = (async function (collectionName, value) {
   const client = new MongoClient(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -16,13 +16,11 @@ const insert = (async function (collectionName) {
     const db = client.db(dbName);
     let r = await db
       .collection(collectionName)
-      .insertOne({
-        a: 1
-      });
+      .insertOne(value);
   } catch (err) {
     console.log(err.stack);
   }
   client.close();
-})(collectionName);
+})(collectionName, value);
 
 exports.insertToDb = insert;
