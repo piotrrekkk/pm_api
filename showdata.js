91a8e502399a158a74f24281f45f7fff7c13fc98
Piotr Rosa <piotr.rosa@sabre.com>
2020-01-19 16:33:45 +0100

    test

---
+++ showdata.js
@@ -0,0 +1,48 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+const url = 'mongodb://localhost:27017';
+const dbName = 'airQuality';
+
+async function insertToDb(collectionName) {
+    const client = new MongoClient(url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+
+    try {
+
+        await client.connect();
+        const db = client.db(dbName);
+
+
+        var values = {
+            'PM 2.5': [],
+            'PM 10': []
+        };
+        var readpm25 = await db.collection(collectionName)
+            .find({ type: 'PM 2.5' })
+            .toArray((err, item) => {
+                if (!err) {
+                    values['PM 2.5'].push(item)
+                }
+            })
+        return items;
+
+        var readpm10 = await db.collection(collectionName)
+            .find({ type: 'PM 10' })
+            .toArray((err, item) => {
+                if (!err) {
+                    values['PM 10'].push(item)
+                }
+            })
+        return values;
+
+
+    } catch (err) {
+        console.log(err.stack);
+    }
+    client.close();
+};
+
+exports.showData = showData;
