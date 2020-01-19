f2c2ece3b47b2f0ae7324b3ca2562748bdc65cb7
Piotr Rosa <piotr.rosa@sabre.com>
2020-01-19 16:57:54 +0100

    test

--- showdata.js
+++ showdata.js
@@ -14,28 +14,15 @@ async function showData(collectionName) {
         await client.connect();
         const db = client.db(dbName);
 
-        var values = {
-            'PM 2.5': [],
-            'PM 10': []
-        };
         return Promise.all(
             db.collection(collectionName)
                 .find({ type: 'PM 2.5' })
                 .toArray((err, item) => {
-                    console.log('toArray2.5', item)
-                    if (!err) {
-                        values['PM 2.5'].push(item)
-                    }
                     return item;
                 }),
-
             db.collection(collectionName)
                 .find({ type: 'PM 10' })
                 .toArray((err, item) => {
-                    console.log('toArray10', item)
-                    if (!err) {
-                        values['PM 10'].push(item)
-                    }
                     return item;
                 })
 
